cb27810ae85a1599c5d6660ee241f52ac22f7665
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-09-16 22:45:38 +0100

    feat: Mint a PERC-20 token

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,16 @@
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-  /**
-   * @dev make sure the first argument has the same name as your contract in the Hello_swtr.sol file
-   * @dev the second argument must be the message we want to set in the contract during the deployment process
-   */
-  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik!!"]);
-
-  await contract.waitForDeployment();
-
-  console.log(`Swisstronik contract deployed to ${contract.target}`);
+  const perc20 = await ethers.deployContract("PERC20Sample");
+  await perc20.waitForDeployment();
+  const deployedContract = await perc20.getAddress();
+  fs.writeFileSync("contract.txt", deployedContract);
+  
+  console.log(`PERC20Sample was deployed to: ${deployedContract}`)
 }
 
-//DEFAULT BY HARDHAT:
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
-}); 
+});
